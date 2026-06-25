83ef9f8ed8640949b60b634d78efca1af61375a0
蛇道 <100847059+caminodelaserpiente@users.noreply.github.com>
2026-06-24 19:02:27 -0600

    ADD: main.py with Streamlit app

---
+++ main.py
@@ -0,0 +1,22 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Repository: https://github.com/caminodelaserpiente/BrailleBook
+
+
+import streamlit as st
+from src.templates.body import config_page, body
+
+
+config_page()
+body()
